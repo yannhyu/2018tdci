af4ab9546d38a95770a438d8d60060044e9777e3
Ian Yu <ianyu_it@yahoo.com>
2018-03-01 21:46:05 -0500

    add n days ago

---
+++ dates_utils/n_days_ago.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+
+N = 2
+
+date_N_days_ago = datetime.now() - timedelta(days=N)
+
+print(datetime.now())
+print(type(datetime.now()))
+print(date_N_days_ago)
+
+N = 3
+string_date = '2018-03-01'
+dt_string_date = datetime.strptime(string_date, "%Y-%m-%d")
+dt_N_days_ago = dt_string_date - timedelta(days=N)
+print(dt_N_days_ago)
+print(dt_N_days_ago.strftime('%Y-%m-%d'))
